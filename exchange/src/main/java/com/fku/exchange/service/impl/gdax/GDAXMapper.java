e4a1cd21a35cb971a1e62755861fd20ce546f781
Filip Kucera <kucerafilip@gmail.com>
2018-02-09 22:42:24 +0100

    gdax candet to time series mapping

--- exchange/src/main/java/com/fku/exchange/service/impl/gdax/GDAXMapper.java
+++ exchange/src/main/java/com/fku/exchange/service/impl/gdax/GDAXMapper.java
@@ -3,26 +3,31 @@ package com.fku.exchange.service.impl.gdax;
 import com.fku.exchange.service.impl.gdax.dto.GDAXHistoricRates;
 import org.ta4j.core.*;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class GDAXMapper {
-    public static TimeSeries remap(GDAXHistoricRates[] gdaxHistoricRates) {
+    public static TimeSeries remap(GDAXHistoricRates[] gdaxHistoricRates, long durationInSecond) {
         List<Bar> ticks = Arrays.stream(gdaxHistoricRates)
+                .sorted(Comparator.comparing(GDAXHistoricRates::getTime)) // time series must be sorted (eg: 0: 2017-07-01T17:00+02:00, 1: 2017-07-01T17:01+02:00 ...)
                 .map(rates -> {
                             long epochInMilliseconds = rates.getTime() * 1000; // seconds-based epoch value needs to be converted to milliseconds
-                            ZonedDateTime tradeTimestamp = ZonedDateTime.ofInstant(Instant.ofEpochMilli(epochInMilliseconds), ZoneId.systemDefault());
+                            ZonedDateTime startTime = ZonedDateTime.ofInstant(Instant.ofEpochMilli(epochInMilliseconds), ZoneId.systemDefault());
                             return new BaseBar(
-                                    tradeTimestamp,
-                                    rates.getOpen(),
-                                    rates.getHigh(),
-                                    rates.getLow(),
-                                    rates.getClose(),
-                                    rates.getVolume()
+                                    Duration.ofSeconds(durationInSecond),
+                                    startTime.plusSeconds(durationInSecond),
+                                    Decimal.valueOf(rates.getOpen()),
+                                    Decimal.valueOf(rates.getHigh()),
+                                    Decimal.valueOf(rates.getLow()),
+                                    Decimal.valueOf(rates.getClose()),
+                                    Decimal.valueOf(rates.getVolume())
                             );
                         }
                 )
--- exchange/src/test/java/com/fku/exchange/service/impl/gdax/GDAXMapperTest.java
+++ exchange/src/test/java/com/fku/exchange/service/impl/gdax/GDAXMapperTest.java
@@ -10,8 +10,6 @@ import org.ta4j.core.TimeSeries;
 
 import java.io.InputStream;
 import java.time.Duration;
-import java.time.Instant;
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 import static org.assertj.core.api.Assertions.*;
@@ -21,9 +19,10 @@ public class GDAXMapperTest {
     public void remap_historicRates() throws Exception {
         // given
         // Sample date contains:
-        // first date 1498924740 -> 2017-07-01T17:59+02:00[Europe/Prague]
-        // one minutes interval between buckets
-        // last  date 1498921200 -> 2017-07-01T17:00+02:00[Europe/Prague]
+        // first bucket start date 1498924740 -> 2017-07-01T17:59+02:00[Europe/Prague] (last bar in time series)
+        // second bucket start date 1498924680 -> 2017-07-01T17:58+02:00[Europe/Prague]
+        // third bucket start date 1498924620 -> 2017-07-01T17:57+02:00[Europe/Prague]
+        // last bucket start  date 1498921200 -> 2017-07-01T17:00+02:00[Europe/Prague]
         // https://api.gdax.com/products/BTC-EUR/candles?start=2017-07-01T10%3A00%3A00.000000-0500&end=2017-07-01T11%3A00%3A00.000000-0500&granularity=60
 
         InputStream is = GDAXHistoricRatesTest.class.getResourceAsStream("/2017-07-01_product_candles_minute_interval.json");
@@ -31,24 +30,40 @@ public class GDAXMapperTest {
         GDAXHistoricRates[] gdaxHistoricRates = mapper.readValue(is, GDAXHistoricRates[].class);
 
         // when
-        TimeSeries timeSeriesTested = GDAXMapper.remap(gdaxHistoricRates);
+        TimeSeries timeSeriesTested = GDAXMapper.remap(gdaxHistoricRates, 60); // one minutes interval between buckets
 
         // than
-        assertThat(false).isTrue(); // TODO vyzkoumat casy v time series
+        assertThat(timeSeriesTested.getFirstBar())
+                .extracting(Bar::getBeginTime,
+                            Bar::getEndTime,
+                            Bar::getTimePeriod)
+                .as("First must be the earliest bar")
+                .contains(  ZonedDateTime.parse("2017-07-01T17:00+02:00[Europe/Prague]"), // 1498921200 -> 2017-07-01T17:00+02:00[Europe/Prague]
+                            ZonedDateTime.parse("2017-07-01T17:01+02:00[Europe/Prague]"),
+                            Duration.ofSeconds(60));
+
+        assertThat(timeSeriesTested.getFirstBar())
+                .as("Historical data from GDAX (candles) must be remapped time series correctly")
+                .extracting(Bar::getOpenPrice, Bar::getMaxPrice, Bar::getMinPrice, Bar::getClosePrice, Bar::getVolume)
+                .contains(Decimal.valueOf(2161.91), Decimal.valueOf(2161.91), Decimal.valueOf(2161.91), Decimal.valueOf(2161.91), Decimal.valueOf(0.14));
+
+
         assertThat(timeSeriesTested.getLastBar())
                 .extracting(Bar::getBeginTime,
                             Bar::getEndTime,
                             Bar::getTimePeriod)
-                .contains(  ZonedDateTime.parse("2017-06-30T17:00+02:00[Europe/Prague]"),
-                            ZonedDateTime.parse("2017-07-01T17:00+02:00[Europe/Prague]"),
-                            Duration.ofSeconds(86400));
+                .as("Last must be the latest bar")
+                .contains(  ZonedDateTime.parse("2017-07-01T17:59+02:00[Europe/Prague]"), // 1498924740 -> 2017-07-01T17:59+02:00[Europe/Prague]
+                            ZonedDateTime.parse("2017-07-01T18:00+02:00[Europe/Prague]"),
+                            Duration.ofSeconds(60));
+
+        assertThat(timeSeriesTested.getLastBar())
+                .as("Historical data from GDAX (candles) must be remapped time series correctly")
+                .extracting(Bar::getOpenPrice, Bar::getMaxPrice, Bar::getMinPrice, Bar::getClosePrice, Bar::getVolume)
+                .contains(Decimal.valueOf(2163.3), Decimal.valueOf(2163.3), Decimal.valueOf(2163.3), Decimal.valueOf(2163.3), Decimal.valueOf(0.04));
 
         assertThat(timeSeriesTested.getBarData())
                 .as("In series from 2017-07-01T17:00 to 2017-07-01T17:59 with granularity 60 seconds must be 59 samples")
                 .hasSize(59);
-        assertThat(timeSeriesTested.getFirstBar())
-                .as("Historical data from GDAX (candles) must be remapped time series correctly")
-                .extracting(Bar::getOpenPrice, Bar::getMaxPrice, Bar::getMinPrice, Bar::getClosePrice, Bar::getVolume)
-                .contains(Decimal.valueOf(2163.3), Decimal.valueOf(2163.3), Decimal.valueOf(2163.3), Decimal.valueOf(2163.3), Decimal.valueOf(0.04));
     }
 }
